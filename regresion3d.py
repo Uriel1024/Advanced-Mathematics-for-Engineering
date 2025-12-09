49f8a2df85f308a5590b3c0af96919af828853dc
Uriel Velazquez <evelazqueza1900@alumno.ipn.mx>
2025-12-09 11:15:58 -0600

    Final version

--- regresion3d.py
+++ regresion3d.py
@@ -27,7 +27,7 @@ gasto = [24.6,33,36.6,39.8,28.6]
 cachorros = [1,3,4,4,2]
 raciones = [11,13,13,14,12]
 
-c0,c1,c2,paso,tol,ite = -4, 3,2,.01,0.00001,1
+c0,c1,c2,paso,tol,ite = -4, 3,2,.01,.2,1
 c0f = get_c0(paso,c0,c1,c2)
 c1f = get_c1(paso,c0,c1,c2)
 c2f = get_c2(paso,c0,c1,c2)
@@ -45,7 +45,7 @@ while (abs(c1f - c1) >= tol)  or (abs(c0f - c0) >= tol) or (abs( c2f  - c2) >= t
     c0f = get_c0(paso,c0,c1,c2)
     c1f = get_c1(paso,c0,c1,c2)
     c2f = get_c2(paso,c0,c1,c2)
-
+    
         
     print(f"El valor de c0 en es de {c0f} con un error de {abs(c0f - c0)}, en la iteracion {ite}")
     print(f"El valor de c1 en es de {c1f} con un error de {abs(c1f - c1)}, en la iteracion {ite}")
